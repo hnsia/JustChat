d262861d6a4ce3fcd7f6b795033d90958969110f
hnsia <44498178+hnsia@users.noreply.github.com>
2023-06-12 00:24:30 +0800

    Add currently login user on sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
-import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
 
-const DesktopSidebar = () => {
+import useRoutes from "@/app/hooks/useRoutes";
+
+import DesktopItem from "./DesktopItem";
+import Avatar from "../Avatar";
+
+interface DesktopSidebarProps {
+  currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
@@ -27,6 +35,15 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
