662c1067cd7606ca73d4e211714c79fcbf787ee3
hnsia <44498178+hnsia@users.noreply.github.com>
2023-06-29 22:47:42 +0800

    Implement dynamic active and offline statuses

---
+++ app/components/ActiveStatus.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import useActiveChannel from "../hooks/useActiveChannel";
+
+interface ActiveStatusProps {}
+
+const ActiveStatus: React.FC<ActiveStatusProps> = ({}) => {
+  useActiveChannel();
+
+  return null;
+};
+
+export default ActiveStatus;
